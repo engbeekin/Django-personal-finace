ad661326f5c3a7e9ab265830e381ec3f151d323a
MSI\beeki <khalidbeekin1@gmail.com>
2025-01-08 10:24:43 +0000

    implemented account balance updated after transaction amount is changed

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.shortcuts import render, redirect, get_object_or_404
@@ -8,11 +10,13 @@ from accounts.models import Account
 from .form import TransactionForm
 from .models import Transaction
 
+
 @login_required
 def index(request):
     transactions = Transaction.objects.select_related('user', 'account', 'category').order_by('-id')
 
     return render(request, 'index.html', {'transactions': transactions})
+
 
 @login_required
 def income_transactions(request):
@@ -21,12 +25,14 @@ def income_transactions(request):
 
     return render(request, 'income-list.html', {'transactions': transactions})
 
+
 @login_required
 def expense_transactions(request: object) -> object:
     transactions = (Transaction.objects.filter(type_of_transaction='expense')
                     .select_related('user', 'account', 'category').all())
 
     return render(request, 'expense-list.html', {'transactions': transactions})
+
 
 @login_required
 @transaction.atomic
@@ -46,14 +52,18 @@ def create(request) -> object:
     form = TransactionForm()
     return render(request, 'create.html', {'form': form})
 
+
 @login_required
 @transaction.atomic
 def update(request, transaction_id: int):
     transaction = get_object_or_404(Transaction, pk=transaction_id)
     form = TransactionForm(request.POST or None, instance=transaction)
+    old_transaction_amount = transaction.Amount
+    print(f'transaction db amount: {transaction.Amount}')
+    print(f'transaction request amount: {old_transaction_amount}')
     if form.is_valid():
         try:
-            process_transaction(request, form)
+            process_transaction(request, form, old_transaction_amount)
             messages.success(request, 'Transaction Updated successfully!')
             return redirect('transactions')
         except Exception as e:
@@ -61,6 +71,7 @@ def update(request, transaction_id: int):
                           {'form': form, 'transaction': transaction, 'error': e})
 
     return render(request, 'edit.html', {'form': form, 'transaction': transaction})
+
 
 @login_required
 @transaction.atomic
@@ -72,10 +83,12 @@ def delete(request, transaction_id: int):
     return redirect('/')
 
 
-def process_transaction(request, form):
+def process_transaction(request, form, old_transaction_amount=0):
     account_id = form.cleaned_data['account'].id
-    transaction_amount = form.cleaned_data['Amount']
     transaction_type = form.cleaned_data['type_of_transaction']
+
+    # updated the account balance after transaction is updated
+    transaction_amount = abs(Decimal(old_transaction_amount) - form.cleaned_data['Amount'])
     account = Account.objects.get(pk=account_id)
 
     # validate the transaction amount if exceed the account balance
